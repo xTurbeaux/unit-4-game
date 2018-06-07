7a1c239307adfc3ac1d13875248fd00e26a69c01
aaron mayhew <aaronmayhew@10-69-9-223.lawrencefreenet.org>
2018-06-07 17:22:00 -0500

    maorrrrrr

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+
+var random_r;
+var win;
+var lost;
+
+for(var i = 0; i < 4; i++){
+    
+    var crystal = $("<div>");
+    $(".crystals").append(crystal);
+    
+}
+
+
