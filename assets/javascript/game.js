8f9c63e50d60cc183733ac341d37330b463aec6e
aaron mayhew <aaronmayhew@10-69-9-223.lawrencefreenet.org>
2018-06-07 20:22:51 -0500

    MOARRR

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,20 @@
 var random_r;
 var win;
 var lost;
+var last = 0;
+
+
+
+$(".crystal").attr('class');
 
 random_r = Math.floor(Math.random() * 120 ) + 12;
     console.log(random_r);
 
-$("#Random_result").html('Random Result: ');
+$("#Random_result").html('Random Result: ' + random_r);
 
 for(var i = 0; i < 4; i++){
 
-    var random = Math.floor(Math.random() * 12);
+    var random = Math.floor(Math.random() * 12) + 1;
     console.log(random);
     var crystal = $("<div>");
         crystal.attr({
@@ -22,4 +27,21 @@ for(var i = 0; i < 4; i++){
     
 }
 
+$(".crystal").on('click', function() {
+    
+    var num = parseInt($(this).attr('data-random'));
+
+    last += num;
+
+    console.log(last);
+
+    if(last > random_r) {
+        console.log("lose");
+    }
+    else if(last === random_r){
+        console.log("win");
+    }
+    
+
+});
 
